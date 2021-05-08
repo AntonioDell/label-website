a47801b9df7902b1a9e1dae9b09c527ad927baff
Antonio Dell'Annunziata <antonio.dell@gmx.de>
2021-05-08 11:40:22 +0200

    Error handling for markdown files

--- src/repository/markdown.js
+++ src/repository/markdown.js
@@ -1,14 +1,22 @@
 import { onMounted, ref } from "vue";
 
-const loadMarkdownFile = async (fileName) => {
-  const { default: file } = await require("@/assets/md/" + fileName);
-  return file;
+const loadMarkdownFileContent = async (fileName) => {
+  try {
+    const { default: fileContent } = await require("@/assets/md/" + fileName);
+    return fileContent;
+  } catch (error) {
+    return (
+      "⚠️ Failed to load markdown file **" +
+      fileName +
+      "**. Please check the file name in /assets/md/."
+    );
+  }
 };
 
 export function useMarkdown(fileName) {
   const markdown = ref();
   const getMarkdown = async () => {
-    markdown.value = await loadMarkdownFile(fileName);
+    markdown.value = await loadMarkdownFileContent(fileName);
   };
 
   onMounted(getMarkdown);
